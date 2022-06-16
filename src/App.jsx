f7aaf71c10026d66130597becfe9a7cf21afb242
vasiliy <wizardcatt@gmail.com>
2022-06-16 23:00:51 +0300

    timer изучение жизненного цикла

---
+++ src/App.jsx
@@ -0,0 +1,85 @@
+import React, { Component } from "react";
+import './App.css'
+
+class App extends  Component {
+    state = {
+      count: 0,
+      isCounting: false,
+      counter: 0
+    };
+
+    componentDidMount() {
+        const userCount = localStorage.getItem("timer");
+        if(userCount) {
+            this.setState({counter: +userCount});
+        }
+    }
+
+    componentDidUpdate() {
+        localStorage.setItem('timer', this.state.counter);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.counterInterval);
+    }
+
+    increaseClick = () => {
+        this.setState({
+            count: this.state.count + 1
+        })
+    };
+
+    decreaseClick = () => {
+        this.setState({
+            count: this.state.count - 1
+        })
+    };
+
+
+    timerStart = () => {
+        this.setState({isCounting: true})
+
+        this. counterInterval = setInterval(() => {
+            this.setState({
+                counter: this.state.counter + 1
+            })
+        })
+    }
+
+    timerStop = () => {
+        this.setState({isCounting: false})
+        clearInterval(this.counterInterval)
+    }
+
+    timerReset = () => {
+        this.setState({isCounting: false, counter: 0})
+        clearInterval(this.counterInterval)
+    }
+
+
+
+
+
+
+    render() {
+        return (
+            <div className="App">
+                <button onClick={this.increaseClick}>+</button>
+                  <p>{this.state.count}</p>
+                  <button onClick={this.decreaseClick}>-</button>
+                <br/>
+                <h2>React Timer</h2>
+                <p>{this.state.counter}</p>
+                {!this.state.isCounting ? (
+                    <button onClick={this.timerStart}>Start</button> ) : (
+                    <button onClick={this.timerStop}>Stop</button>
+                )}
+                <button onClick={this.timerReset}>Reset</button>
+
+
+            </div>
+        );
+    }
+}
+
+export default App;
